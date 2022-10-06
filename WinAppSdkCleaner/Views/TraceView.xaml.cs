8aa1246a0dee88c018ec5ce3e84640c1b6c0401f
David Hancock <code@davidhancock.net>
2022-10-06 12:05:24 +0100

    Add a Trace output tab
    I'm too lazy to attach a debugger. Also simplify the sdk view layout so it matches the new tab.

---
+++ WinAppSdkCleaner/Views/TraceView.xaml.cs
@@ -0,0 +1,69 @@
+﻿namespace WinAppSdkCleaner.Views;
+
+/// <summary>
+/// Interaction logic for TraceView.xaml
+/// </summary>
+public partial class TraceView : UserControl
+{
+    private readonly ViewCommand clearCommand;
+
+    public TraceView()
+    {
+        InitializeComponent();
+
+        clearCommand = InitialiseCommand("Clear", ExecuteClear, CanClear);
+
+        Trace.Listeners.Add(new CustomTraceListener(TraceTextBox));
+
+        Loaded += (s, a) => AdjustCommandsState();
+    }
+
+    private ViewCommand InitialiseCommand(string key, Action<object?> execute, Func<object?, bool> canExecute)
+    {
+        ViewCommand command = (ViewCommand)FindResource(key);
+        command.CanExecuteProc = canExecute;
+        command.ExecuteProc = execute;
+        return command;
+    }
+
+    public void ExecuteClear(object? param = null) => TraceTextBox.Clear();
+
+    private bool CanClear(object? param = null) => TraceTextBox.Text.Length > 0;
+
+    private void AdjustCommandsState() => clearCommand.RaiseCanExecuteChanged();
+
+    private void TextChanged(object sender, TextChangedEventArgs e) => AdjustCommandsState();
+
+
+    internal class CustomTraceListener : TraceListener
+    {
+        private TextBox Consumer { get; init; }
+
+        public CustomTraceListener(TextBox consumer)
+        {
+            Consumer = consumer;
+        }
+
+        public override void Write(string? message)
+        {
+            if (message is not null)
+            {
+                Consumer.Dispatcher.Invoke(() =>
+                {
+                    int selectionStart = Consumer.SelectionStart;
+                    int selectionLength = Consumer.SelectionLength;
+                    
+                    Consumer.Text += message;
+
+                    if (selectionLength > 0)
+                        Consumer.Select(selectionStart, selectionLength);
+                    else
+                        Consumer.CaretIndex = Consumer.Text.Length;
+                });
+            }
+        }
+
+        public override void WriteLine(string? message) => Write(message + Environment.NewLine);
+    }
+}
+
