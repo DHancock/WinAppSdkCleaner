e16bdb834db3f5855a83388140a0b462bfdb46c0
David Hancock <code@davidhancock.net>
2022-10-25 22:11:15 +0100

    Tidy up
    A function pointer wasn't needed. Change file name so it isn't the same as the enum type name.

---
+++ WinAppSdkCleaner/Models/Sdks.cs
@@ -0,0 +1,37 @@
+﻿namespace WinAppSdkCleaner.Models;
+
+internal interface ISdk
+{
+    string DispalyName { get; }
+
+    bool Match(PackageId pId);
+
+    SdkTypes TypeId { get; }
+}
+
+
+internal sealed class ProjectReunion : ISdk
+{
+    public string DispalyName => "Project Reunion";
+
+    public bool Match(PackageId pId)
+    {
+        return pId.FullName.Contains("ProjectReunion", StringComparison.OrdinalIgnoreCase);
+    }
+   
+    public SdkTypes TypeId => SdkTypes.Reunion;
+}
+
+internal sealed class WinAppSdk : ISdk
+{
+    public string DispalyName => "Windows App SDK";
+
+    public bool Match(PackageId pId)
+    {
+        return pId.FullName.Contains("WinAppRuntime", StringComparison.OrdinalIgnoreCase) ||
+                pId.FullName.Contains("WindowsAppRuntime", StringComparison.OrdinalIgnoreCase) ||
+                pId.FullName.StartsWith("Microsoft.WindowsAppSDK", StringComparison.Ordinal);  // for 1.0.0 experimental 1 only
+    }
+
+    public SdkTypes TypeId => SdkTypes.WinAppSdk;
+}
