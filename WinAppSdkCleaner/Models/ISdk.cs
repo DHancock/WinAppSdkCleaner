196bf8b2e3abb257b9d1de4b76597fa697ecae07
David Hancock <code@davidhancock.net>
2022-10-26 10:32:35 +0100

    Tidy up
    Mostly splitting ISdk interface and classes into separate files.

---
+++ WinAppSdkCleaner/Models/ISdk.cs
@@ -0,0 +1,13 @@
+﻿namespace WinAppSdkCleaner.Models;
+
+public enum SdkId { Reunion, WinAppSdk };
+
+internal interface ISdk
+{
+    string DispalyName { get; }
+
+    bool Match(PackageId pId);
+
+    SdkId Id { get; }
+}
+
