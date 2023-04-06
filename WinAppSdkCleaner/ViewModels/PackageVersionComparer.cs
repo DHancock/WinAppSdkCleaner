46204ec56ad2a42e470769a19d3173dadf0d68f7
David Hancock <code@davidhancock.net>
2023-04-06 15:46:50 +0100

    Display package version info
    Might as well. Could be useful if you're writing an installer and need to determine what's currently installed.

---
+++ WinAppSdkCleaner/ViewModels/PackageVersionComparer.cs
@@ -0,0 +1,24 @@
+﻿namespace WinAppSdkCleaner.ViewModels;
+
+internal sealed class PackageVersionComparer : IComparer<PackageVersion>
+{
+    public int Compare(PackageVersion a, PackageVersion b)
+    {
+        int result = a.Major - b.Major;
+
+        if (result == 0)
+        {
+            result = a.Minor - b.Minor;
+
+            if (result == 0)
+            {
+                result = a.Build - b.Build;
+
+                if (result == 0)
+                    result = a.Revision - b.Revision;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ WinAppSdkCleaner/Views/VersionsView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using WinAppSdkCleaner.ViewModels;
+
+namespace WinAppSdkCleaner.Views;
+
+/// <summary>
+/// Interaction logic for VersionsView.xaml
+/// </summary>
+public partial class VersionsView : UserControl
+{
+    public VersionsView()
+    {
+        InitializeComponent();
+
+        DataContext = new VersionsViewModel();
+
+        Loaded += async (s, e) =>
+        {
+            await ((VersionsViewModel)DataContext).LoadVersionInfo();
+        };
+    }
+}
+
+
