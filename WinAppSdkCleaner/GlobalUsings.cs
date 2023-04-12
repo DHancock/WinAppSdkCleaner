4c57da033de01cd1092ef9988730de0e468db153
David Hancock <code@davidhancock.net>
2023-04-12 16:30:57 +0100

    Improve error text

--- WinAppSdkCleaner/GlobalUsings.cs
+++ WinAppSdkCleaner/GlobalUsings.cs
@@ -9,6 +9,7 @@ global using System.Linq;
 global using System.Net.Http;
 global using System.Reflection;
 global using System.Runtime.CompilerServices;
+global using System.Runtime.InteropServices;
 global using System.Security.Principal;
 global using System.Text;
 global using System.Text.Json;
