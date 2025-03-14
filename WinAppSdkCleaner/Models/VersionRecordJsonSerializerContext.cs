61263a47259daa6ff8188516917d9465d1774c00
David Hancock <code@davidhancock.net>
2025-03-14 10:19:56 +0000

    Remove unnecessary json serializer code
    The list of version records also generates the version record code

--- WinAppSdkCleaner/Models/VersionRecordJsonSerializerContext.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WinAppSdkCleaner.Models;
-
-[JsonSourceGenerationOptions(IncludeFields = true, WriteIndented = false)]
-[JsonSerializable(typeof(VersionRecord))]
-internal sealed partial class VersionRecordJsonSerializerContext : JsonSerializerContext
-{
-}
